a501dc853a224f202cc86441984306e64fbe59e8
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2021-12-24 20:57:11 +0330

    Remove hooks

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -10,11 +10,6 @@
  * @link https://github.com/respinar/contao-voucher-bundle
  */
 
-//use Respinar\ContaoVoucherBundle\EventListener\StoreFormDataListener;
-//use Respinar\ContaoVoucherBundle\EventListener\PrepareFormDataListener;
-//use Respinar\ContaoVoucherBundle\EventListener\ValidateFormFieldListener;
-use Respinar\ContaoVoucherBundle\EventListener\ProcessFormDataListener;
-
 use Respinar\ContaoVoucherBundle\Model\VoucherAcceptorModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherGiftModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherCardModel;
@@ -26,8 +21,7 @@ use Respinar\ContaoVoucherBundle\Model\VoucherStaffModel;
  * Backend modules
  */
 $GLOBALS['BE_MOD']['voucher_module'] = array (
-    'voucher_gift' => array( 'tables'=> array('tl_voucher_gift')),
-    'voucher_card' => array( 'tables'=> array('tl_voucher_card')),
+    'voucher_card' => array( 'tables'=> array('tl_voucher_card','tl_voucher_gift')),
     'voucher_staff' => array( 'tables'=> array('tl_voucher_staff')),
     'voucher_acceptor' => array( 'tables'=> array('tl_voucher_acceptor')),
     'voucher_log' => array( 'tables'=> array('tl_voucher_log'))
@@ -40,9 +34,3 @@ $GLOBALS['TL_MODELS']['tl_voucher_card']     = VoucherCardModel::class;
 $GLOBALS['TL_MODELS']['tl_voucher_gift']     = VoucherGiftModel::class;
 $GLOBALS['TL_MODELS']['tl_voucher_staff']    = VoucherStaffModel::class;
 $GLOBALS['TL_MODELS']['tl_voucher_acceptor'] = VoucherAcceptorModel::class;
-
-
-//$GLOBALS['TL_HOOKS']['storeFormData'][] = [StoreFormDataListener::class, '__invoke'];
-//$GLOBALS['TL_HOOKS']['prepareFormData'][] = [PrepareFormDataListener::class, '__invoke'];
-//$GLOBALS['TL_HOOKS']['validateFormField'][] = [ValidateFormFieldListener::class, '__invoke'];
-$GLOBALS['TL_HOOKS']['processFormData'][] = [ProcessFormDataListener::class, '__invoke'];
