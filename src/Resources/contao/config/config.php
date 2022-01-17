e071cbb32492edf86c969970c59a88ffc681a9b7
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2022-01-17 20:59:07 +0330

    Add new DCA

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -14,12 +14,10 @@ use Respinar\ContaoVoucherBundle\Model\VoucherAcceptorModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherGiftModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherCardModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherStaffModel;
-use Respinar\ContaoVoucherBundle\Model\VoucherGatewayModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherLogModel;
 use Respinar\ContaoVoucherBundle\Model\VoucherInvoiceModel;
-
-
-
+use Respinar\ContaoVoucherBundle\Model\VoucherSMSGatewayModel;
+use Respinar\ContaoVoucherBundle\Model\VoucherSMSLogModel;
 
 
 /**
@@ -30,16 +28,17 @@ $GLOBALS['BE_MOD']['voucher_module'] = array (
     'voucher_staff' => array( 'tables'=> array('tl_voucher_staff')),
     'voucher_acceptor' => array( 'tables'=> array('tl_voucher_acceptor')),
     'voucher_invoice' => array( 'tables'=> array('tl_voucher_invoice')),
-    'voucher_gateway' => array( 'tables'=> array('tl_voucher_gateway'))
+    'voucher_gateway' => array( 'tables'=> array('tl_voucher_sms_gateway'))
 );
 
 /**
  * Models
  */
-$GLOBALS['TL_MODELS']['tl_voucher_card']     = VoucherCardModel::class;
-$GLOBALS['TL_MODELS']['tl_voucher_gift']     = VoucherGiftModel::class;
-$GLOBALS['TL_MODELS']['tl_voucher_staff']    = VoucherStaffModel::class;
-$GLOBALS['TL_MODELS']['tl_voucher_acceptor'] = VoucherAcceptorModel::class;
-$GLOBALS['TL_MODELS']['tl_voucher_invoice']  = VoucherInvoiceModel::class;
-$GLOBALS['TL_MODELS']['tl_voucher_log']      = VoucherLogModel::class;
-$GLOBALS['TL_MODELS']['tl_voucher_gateway']  = VoucherGatewayModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_card']        = VoucherCardModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_gift']        = VoucherGiftModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_staff']       = VoucherStaffModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_acceptor']    = VoucherAcceptorModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_invoice']     = VoucherInvoiceModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_log']         = VoucherLogModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_sms_gateway'] = VoucherSMSGatewayModel::class;
+$GLOBALS['TL_MODELS']['tl_voucher_sms_log']     = VoucherSMSLogModel::class;
