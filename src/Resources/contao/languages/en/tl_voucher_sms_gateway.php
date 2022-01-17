86681292f3ed604fc0e66c6fa4c7f03a857a1208
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2022-01-17 20:58:24 +0330

    Rename

---
+++ src/Resources/contao/languages/en/tl_voucher_sms_gateway.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Voucher manager.
+ * 
+ * (c) Hamid Abbaszadeh 2021 <abbaszadeh.h@gmail.com>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-voucher-bundle
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['title_legend'] = "Title";
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['config_legend'] = "Configration";
+
+/**
+ * Operations
+ */
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['edit'] = ["Card mit ID: %s bearbeiten", "Card mit ID: %s bearbeiten"];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['delete'] = ["Card mit ID: %s löschen", "Card mit ID: %s löschen"];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['show'] = ["Card mit ID: %s ansehen", "Card mit ID: %s ansehen"];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['new'] = ["New Gateway", "Create New SMS Gateway"];
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['tstamp'] = ["Issue date", ""];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['title'] = ["Title", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['gateway'] = ["Gateway", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['fromNumber'] = ["From Number", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['username'] = ["Username", "Please enter username."];
+$GLOBALS['TL_LANG']['tl_voucher_sms_gateway']['password'] = ["Password", "Please enter password."];
